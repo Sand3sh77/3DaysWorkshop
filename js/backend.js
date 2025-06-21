2cbbbdb7cb81a4e29b36d26e416f716d63d770a2
sand3sh77 <subedisandesh0123@gmail.com>
2025-06-21 19:00:44 +0545

    Update payment route in backend.js to use the correct endpoint for payment processing

--- js/backend.js
+++ js/backend.js
@@ -1,5 +1,6 @@
 export const backendURL = "http://localhost:1111";
 
 export const userRoute = backendURL + "/users";
-export const productRoute = backendURL + "/products";export const paymentRoute = backendURL + "/pay/esewa-v2";
+export const productRoute = backendURL + "/products";
+export const paymentRoute = backendURL + "/pay/esewa";
 export const orderRoute = backendURL + "/orders";
